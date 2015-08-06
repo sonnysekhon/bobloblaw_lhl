f86630ede01111c8f6116d897525d6e8d700720c
sonnysekhon <sonnysekhon@gmail.com>
2015-08-06 10:35:45 -0700

    Scraping Working With Nokogiri

---
+++ test.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+url = "http://www.yellowpages.ca/search/si/1/gym/Vancouver%2C%20BC"
+url = "http://www.yellowpages.ca/search/si/1/gym/Vancouver%2C%20BC"
+doc = Nokogiri::HTML(open(url))
+puts doc.at_css("title").text
