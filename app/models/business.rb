20684941577df4f4c449d10c4d0211e118888e95
Joey Burning <joey.byrne@outlook.com>
2015-08-07 16:05:16 -0700

    draft search SQL queries

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,20 @@
 class Business < ActiveRecord::Base
   belongs_to :user
   has_one :layout
+
+
+  protected
+    def self.search(query)
+      where("category LIKE ? OR name LIKE ? or phone LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "AND featured is true")
+      where("category LIKE ? OR name LIKE ? or phone LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "AND featured is false")
+    end
+
+
+    #featured first (true or false)
+
+    #category
+    #name
+    #phone
+    #
+
 end
