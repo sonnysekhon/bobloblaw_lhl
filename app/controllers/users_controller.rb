ac97eae4def1ab58a4a22f4cb11739351de8b924
Joey Burning <joey.byrne@outlook.com>
2015-08-09 15:07:08 -0700

    update user new method in users controller so form renders.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to '/businesses'
     else
       render :new
     end
