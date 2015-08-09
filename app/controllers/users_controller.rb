08896ae6e25e98ba7c241987a7fa313ebcd73965
Joey Burning <joey.byrne@outlook.com>
2015-08-09 15:17:51 -0700

    After sign-up user is redirected to root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/businesses'
+      redirect_to '/'
     else
       render :new
     end
