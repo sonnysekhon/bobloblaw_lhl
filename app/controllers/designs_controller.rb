d5fc11de6eacccb888d5441b52d8b8ba2c2ec5c0
Joey Burning <joey.byrne@outlook.com>
2015-08-12 11:52:30 -0700

    temporary template. calling desgins and rendering based on colours saved in DB

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -1,4 +1,18 @@
 class DesignsController < ApplicationController
+  skip_before_filter :show_navbar
 
+  def new
+    @design = Design.new
+    # @business = Business.find(@design.business_id)
+  end
+
+  def find_design 
+    @design = Design.find_by(business_id: @business.id)
+  end
+
+  def show
+    @design = Design.find(1)
+    @business = Business.find(@design.business_id)
+  end
 
 end
