36e3c192719d06019e6aa874502694e5bd69a125
Joey Burning <joey.byrne@outlook.com>
2015-08-13 15:33:30 -0700

    transfer tested dropdown colour selection Javascript, CSS, and HTML for designs page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,8 +24,67 @@ var ready;
 ready = function() {
   $('.materialboxed').materialbox();
   $('.parallax').parallax();
-//   $('.design-bar').css("background-color: blue");
+
 }
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+$(function () {
+  $('.colorPickerContainer').hide();
+  $("#firstColor").on('click', function() {
+    $('.mainColor').show("slow");
+  });
+  $("#secondColor").on('click', function() {
+    $('.secondaryColor').show("slow");
+  });
+  $("#accentColor").on('click', function() {
+    $('.accentColor').show("slow");
+  });
+
+
+
+  $('.colorPickerContainer').on('mouseleave', function() {
+    $(this).hide(600);
+  })
+
+  $('.item').on('mousedown', function() {
+    $(this).addClass('clicked-item')
+  });
+  $('.item').on('mouseup', function() {
+    $(this).removeClass('clicked-item')
+  });
+  $('.item').on('mouseleave', function() {
+    $(this).removeClass('clicked-item')
+  });
+
+
+  var selection = {
+
+  };
+
+  $('.item').on('click', function(e)
+  {
+      var x = $(this).css('backgroundColor');
+      var hexColor = hexc(x);
+      var target = $(this).parent().data('color');
+      selection[target] = hexColor;
+      // console.log(hexColor, target);
+  })
+
+  $('button#save').on('click', function()
+  {
+      console.log(selection);
+  });
+
+  function hexc(colorval) {
+      var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      delete(parts[0]);
+      for (var i = 1; i <= 3; ++i) {
+          parts[i] = parseInt(parts[i]).toString(16);
+          if (parts[i].length == 1) parts[i] = '0' + parts[i];
+      }
+      return '#' + parts.join('');;
+  };
+
+});
 
